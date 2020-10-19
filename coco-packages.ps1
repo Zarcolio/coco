6f022434475747d51ddf620c648e841130324435
Zarcolio <registered@quicknet.nl>
2020-10-19 19:18:08 +0200

    Lists packages with version numers and cleans up extra

---
+++ coco-packages.ps1
@@ -0,0 +1,80 @@
+﻿ param (
+    [switch]$List = $false,
+    [switch]$Cleanup = $false
+ )
+
+if ($list -and $cleanup)
+{
+    Write-Host ("Cannot use both -List and -Cleanup at once.")
+    Exit(1)
+}
+
+Function PackagesList($List)
+{
+    $PackageArray = @()
+    $OldPackagesXml = Get-ChildItem $env:ProgramData"\chocolatey\lib" -Directory| Select FullName
+    ForEach ($Folder in $OldPackagesXml)
+    {
+        $ChocoLibFolder = $env:ProgramData + "\chocolatey\lib\chocolatey"
+        If ($Folder.FullName -ne $ChocoLibFolder)
+        {
+            $a = Split-Path ($Folder.FullName) -Leaf
+            $FullFolderPath = $Folder.FullName
+            $FullFilePath = "$FullFolderPath\$a.nuspec"
+            [xml]$app = Get-Content ($FullFilePath)
+            If ($List)
+            {
+                # Param = -List
+                $PackageArray += $app.package.metadata.id + " " + $app.package.metadata.version
+            }
+            Else
+            {
+                # Param = -Cleanup
+                $PackageArray += $env:ProgramData + "\chocolatey\.chocolatey\" + $app.package.metadata.id + "." + $app.package.metadata.version
+            }
+	    }
+    }
+    return $PackageArray
+}
+
+
+$AllPackages = PackagesList($List)
+
+if ($list)
+{
+    ForEach ($Package in $AllPackages)
+    {
+        $Package
+    }
+}
+
+if ($cleanup)
+{
+    $OldPackages = Get-ChildItem $env:ProgramData"\chocolatey\.chocolatey" -Directory| Select FullName
+    $CleanupFolders = @()
+
+    ForEach ($Folder in $OldPackages) 
+    {
+        If ($Folder.FullName -notin $AllPackages)
+        {
+            $CleanupFolders += $Folder.FullName
+        }
+    }
+
+    If ($CleanupFolders.Count -eq 0)
+    {
+        Write-Host ("Nothing to be deleted.")
+    }
+    Else
+    {
+        $FolderDeleteCount = $CleanupFolders.Count
+
+        $Response = Read-Host -Prompt ([String]$FolderDeleteCount + " folders can be deleted. Delete them now? [y/n]")
+        if ($Response.ToLower() -eq 'y') {
+            ForEach ($CleanupFolder in $CleanupFolders)
+            {
+                Remove-Item $CleanupFolder -Recurse
+            }
+        }
+    }
+}
